0717288f5fcb98c9cad8354d81d897134e3561be
awesomesidney <awesomesidney@users.noreply.github.com>
2018-08-08 23:12:01 -0300

    change back approve function

--- contracts/VXR_advanced.sol
+++ contracts/VXR_advanced.sol
@@ -157,23 +157,6 @@ contract VXR is ERC20Interface, Pausable {
         return true;
     }
 
-    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
-        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        return true;
-    }
-
-    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
-        uint oldValue = allowed[msg.sender][_spender];
-        if (_subtractedValue > oldValue) {
-            allowed[msg.sender][_spender] = 0;
-        } else {
-            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-        }
-        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        return true;
-    }
-
     function transferFrom(address from, address to, uint tokens) public whenNotPaused returns (bool success) {
         require(allowed[from][msg.sender] >= tokens);
         _transfer(from, to, tokens);
